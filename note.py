6fd55844a70cd1efc3951fbbf445c6927d1a563d
OGalloy <onedayaslion@gmail.com>
2023-02-14 17:43:41 +0500

    create new methods in Notepad and fixing bugs in note.py

--- note.py
+++ note.py
@@ -1,17 +1,36 @@
 #Заметка
-import datetime
+from datetime import datetime
+
+def parse_time(date):
+    time_last_edition = f"{date.year}-{date.month}-{date.hour}-{date.minute}-{date.second}"
+    return time_last_edition
 
 class Note:
-	def __init__(self, id, title, body, time_last_edition = datetime.datetime.now()):
-		self.id = id
-		self.title = title
-		self.body = body
-		self.time_last_edition = time_last_edition
+    def __init__(self, note_id, title, body, time=parse_time(datetime.now())):
+        self.id = note_id
+        self.title = title
+        self.body = body
+        self.time_last_edition = time 
 
-	def set_title(new_title):
-		self.title = new_title
-		self.time_last_edition = datetime.datetime.now()
+    def set_title(self, new_title):
+        self.title = new_title
+        self.time_last_edition = parse_time(datetime.now())
 
-	def set_body(new_body):
-		self.body = new_body
-		self.time_last_edition = datetime.datetime.now()
+    def set_body(self, new_body):
+        self.body = new_body
+        self.time_last_edition = parse_time(datetime.now())
+
+    def get_time(self):
+        return self.time_last_edition
+
+if __name__ == "__main__":
+    ##code for testing
+    import time
+    n1 = Note(1, "first", "hello")
+    print(n1.id)
+    print(n1.title)
+    print(n1.body)
+    print(n1.time_last_edition)
+    time.sleep(1)
+    n1.set_body("note was changed!!!")
+    print(n1.get_time())
--- notepad.py
+++ notepad.py
@@ -1,17 +1,62 @@
 ##
-import note
+from note import Note
 
 class Notepad:
     def __init__(self, file):
-	    self.notes_list = []
-	    
+        self.notes_list = []
         notes = open(file, "r")
-	    lines = notes.readlines()
-
-	    for line in lines:
+        lines = notes.readlines()
+        for line in lines:
             line = line.split("|")
-            new_note = note.Node(line[0], line[1], line[2], line[3])
+            new_note = Note(int(line[0]), line[1], line[2], line[3])
             self.notes_list.append(new_note)
+        notes.close()
 
-	def get_notes_list()
-        print(self.notes_list)	 
+    def show_all_notes(self):
+        if len(self.notes_list) == 0:
+            print("Notepad is empty!!!")
+        else:
+            print("********List of Notes***********")
+            for n in self.notes_list:
+                print(f"ID: {n.id} | Title: {n.title}")
+
+    def create_note(self, title, body):
+        new_note = Note(len(self.notes_list)+1, title, body,)
+        self.notes_list.append(new_note)
+        print("New note created")
+
+    #read note by id    
+    def read_note(self, id):
+        for n in self.notes_list:
+            if n.id == id:
+                print("*"*30)
+                print(f"ID: {n.id} | Title: {n.title}")
+                print("*"*30)
+                print(f"{n.body}")
+                print(f"LastChange: {n.time_last_edition}")
+
+
+    def update_note(self, id, title, body):
+        for n in self.notes_list:
+            if n.id == id:
+                n.set_title(title)
+                n.set_body(body)
+
+    def delete_note(self, id):
+        for n in self.notes_list:
+            if n.id == id:
+                self.notes_list.remove(n)
+        counter = 1
+        for n in self.notes_list:
+            n.id = counter
+            counter+=1
+
+if __name__ == "__main__":
+    #Code for test
+    np = Notepad("notes.txt")
+    np.show_all_notes()
+    print(np.notes_list[0].id)
+    np.create_note("second note", "hello, world")
+    np.show_all_notes()
+    np.read_note(1)
+    np.read_note(2)
